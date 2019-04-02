971992b02c5715e0acef78c4bc9321ec2d43077d
Jacob Davison <c3324607@uon.edu.au>
2019-04-02 01:58:38 +0000

    Idea for 1st code

--- Main.c
+++ Main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
-int main(void)
+int main()
 {
-    printf("hello, world\n");
-    this is my project, as you can see it is going well
-    lllll
-}
+    char c;
+    printf("Enter Letter: ");
+
+    
+    scanf("%c", &c);  //scans letter//
+    
+ 
+    printf("ASCII value of %c = %d", c, (int)c);
+    return 0;
+}  
+
+//to perform a rotation cipher the assigned ASCII number for each character using c = c-65//
+//thhis will mean that each letter will have an assigned number from A=0 to Z=25//
+//from this use a if loop shown if(number<25) then number++, and if(number=25) then number=0//
