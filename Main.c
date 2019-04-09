48820c280a272b190d56b184f25cae6c370ec81e
Jacob Davison <c3324607@uon.edu.au>
2019-04-09 04:32:12 +0000

    Code that uses the modulus and actually works properly

--- Main.c
+++ Main.c
@@ -1,52 +1,22 @@
 #include <stdio.h>
 int main()
 {
- char b[] = "kbtpo jsfebmf  kbdpc ebwjtpo";
+ char b[] = "NAKNJCJBJCSakjkhkchjcb^^&&^%%$#@";
  int k = 1;
  
  int index;
- int minus;
- int placeHolder;
  
  for(index = 0; b[index] !=0; index++){
-     if(( b[index] < 65) || (b[index] > 90 && b[index] < 97) || (b[index] > 122))  
-     //b[index] > 90) || (b[index] < 97 && b[index] > 122)) //fix this right away as some symbols change to letter
-     {
+     if(( b[index] < 65) || (b[index] > 90 && b[index] < 97) || (b[index] > 122)) {
+       //do nothing to any punctuation  
      }
-     else if (b[index] > 90)
-     {
-      b[index] = b[index] - 32;   
-if((b[index]-k) < 65) 
-{
-   
-   placeHolder =  b[index] - k; //figures out the value for the ascii number minus the k value
-   minus = 65 - placeHolder; //this is the orgi
-   b[index] = 91 - minus; 
-}
- else{
-   b[index] = b[index] - k;   
+     else{
+         b[index] = ((b[index] - 65) + k ) % 26 + 65;
+     }
+         
  }
-    
-}
-else{
- if((b[index]-k) < 65) 
-{
-   
-   placeHolder =  b[index] - k; //figures out the value for the ascii number minus the k value
-   minus = 65 - placeHolder; //this is the o
-   b[index] = 91 - minus; 
-}
- else{
-   b[index] = b[index] - k;     
-}
-// captial things need to go here
-
-}
-}
- 
  printf("encrypted message is %s\n", b);
 }
 
 //to perform a rotation cipher the assigned ASCII number for each character using c = c-32//
-//thhis will mean that each letter will have an assigned number from A=0 to Z=25//
-//from this use a if loop shown if(number<25) then number++, and if(number=25) then number=0//
+//thhis will mean that each letter will have an assigned number from A=0 to Z=25 using the modulus//
