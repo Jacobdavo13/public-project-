ecbc6c3fc8cc04e631c21df692525a0f4e02ae6a
Jacob Davison <c3324607@uon.edu.au>
2019-04-09 01:56:03 +0000

    Revert "converts lower case to upper case and able to stay within the upper cases and convert a to Z"
    This reverts commit f72ede933731fc3086270d94ec6af1dea3d991b8.

--- Main.c
+++ Main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main()
 {
- char b[] = "A";
- int k = 12;
+ char b[] = "abcedfg";
+ int k = 0;
  
  int index;
- int minus;
- int placeHolder;
  
  for(index = 0; b[index] !=0; index++){
      if(b[index] == 32)
@@ -15,27 +13,14 @@ int main()
      else if (b[index] > 90)
      {
       b[index] = b[index] - 32;   
-if((b[index]-k) < 65) 
-{
-   
-   placeHolder =  b[index] - k; //figures out the value for the ascii number minus the k value
-   minus = 65 - placeHolder; //this is the orgi
-   b[index] = 91 - minus; 
+
+    b[index] = b[index] - k;  
 }
- else{
-   b[index] = b[index] - k;   
  }
-    
-}
-// captial things need to go here
-
-}
-}
-
  
  printf("encrypted message is %s\n", b);
 }
 
-//to perform a rotation cipher the assigned ASCII number for each character using c = c-32//
+//to perform a rotation cipher the assigned ASCII number for each character using c = c-65//
 //thhis will mean that each letter will have an assigned number from A=0 to Z=25//
 //from this use a if loop shown if(number<25) then number++, and if(number=25) then number=0//
