4502123eb52cd31a90fb696ac9429a177e3e9575
Jacob Davison <c3324607@uon.edu.au>
2019-04-09 00:38:08 +0000

    Includes new if statement that allows space characters to be left as space characters

--- Main.c
+++ Main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    char c;
-    printf("Enter Letter: ");
-
-    
-    scanf("%c", &c);  //scans letter//
-    
+ char b[] = "dfg hjk";
+ int k = 1;
  
-    printf("ASCII value of %c = %d", c, (int)c);
-    return 0;
-}  
+ int index;
+ 
+ for(index = 0; b[index] !=0; index++){
+     if(b[index] == 32)
+     {
+     }
+     else{
+         
+
+    b[index] = b[index] - k;  
+}
+ }
+ 
+ printf("encrypted message is %s\n", b);
+}
 
 //to perform a rotation cipher the assigned ASCII number for each character using c = c-65//
 //thhis will mean that each letter will have an assigned number from A=0 to Z=25//
