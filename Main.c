caac69488e4f23dc3507bbfbc651ffb66896c2e8
Jacob Davison <c3324607@uon.edu.au>
2019-04-21 12:24:34 +0000

    Finally made it into a function

--- Main.c
+++ Main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+char enc(char b[], int k);
 int main()
 {
- char b[] = "JACOB";
- int k = 1;
+    char message[] = "JACOB";
+    int key =  1;
+enc(message, key);
+return 0;
+}
  
+ char enc(char b[], int k)
+ {
  int index;
- 
  for(index = 0; b[index] !=0; index++){
      if(( b[index] < 65) || (b[index] > 90 && b[index] < 97) || (b[index] > 122)) {
        //do nothing to any punctuation  
@@ -16,7 +21,5 @@ int main()
          
  }
  printf("encrypted message is %s\n", b);
+ return 0;
 }
-
-//to perform a rotation cipher the assigned ASCII number for each character using c = c-32//
-//thhis will mean that each letter will have an assigned number from A=0 to Z=25 using the modulus//
